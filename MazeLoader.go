a24594ec8705ced8c62a5907b3e43d55284938b3
Daniel Franquiz <dfranquiz561@gmail.com>
2020-03-10 18:23:23 -0400

    worked on classes

--- MazeLoader.go
+++ MazeLoader.go
@@ -2,44 +2,37 @@
 
 import "bufio";
 import "os";
-import "fmt"
+import "fmt";
 
-/*		USAGE:
-		mazeLoader := MazeLoader{};
-		// load resources
-		err := mazeLoader.loadMaze("maze01.txt")
-		if err != nil {
-			fmt.Println("failed to load maze:", err)
-			return
-		}
-		mazeLoader.printScreen();
-*/
+
 
 type MazeLoader struct{
 	maze []string;
 }
 
-func (i *MazeLoader) loadMaze(file string) error {
+func (mazeloader *MazeLoader) loadMaze(file string) error {
 	fmt.Println("LOADING MAZE")
-    f, err := os.Open(file)// opening file
-    if err != nil {// if error freak out
-        return err
-    }
-    defer f.Close()// ?????????
-
-    scanner := bufio.NewScanner(f)/// scanner class
-    for scanner.Scan() {
-        line := scanner.Text()
-        i.maze = append(i.maze, line)
-    }
+	//:= short-assignment operator that automatically infers the type of variable(s) based on the value(s) on the right hand side. commonly used in if, for, switch
+	f, err := os.Open(file) //Open function from os package which returns multiple values file and error (common Go pattern)
+	if err != nil { //nil means no value is assigned to a variable
+		return err
+	}
+	defer f.Close() //defer statement defers the execution of a function until the surrounding function returns. in this case we will close the file we opened. puts f.Close in the call stack
+	
+	scanner := bufio.NewScanner(f) //scanner object from bufio to read it to memory(global variable called maze). reads file line by line and appends it to maze slice (clean up purposes)
+	for scanner.Scan() { //scanner.Scan() will return true while there is something to be read from the file
+		line := scanner.Text() //returns the next line of input
+		mazeloader.maze = append(mazeloader.maze, line) //built-in function append adds a new element to the maze slice
+		//slices: lightweight ds, similar to array but more powerful. variable-length sequence which stores elements of similar type. Not a fixed size 
+		//a[low : high]
+	}
+	
 	fmt.Println("Finished loading MAZE")
-    return nil/// returning null
+	return nil
+	//f.Close is called implicitly
 }
 
-func (i *MazeLoader) printScreen() {
-	fmt.Println("PRINTING SCREEN")
-	
-    for _, line := range i.maze {// looping through each line
-        fmt.Println(line)
-    }
+func (mazeloader *MazeLoader) getmaze() []string {
+	maze := mazeloader.maze
+	return maze
 }
--- main.go
+++ main.go
@@ -4,34 +4,12 @@ package main
 //makes code in other packages accessible to this program
 import (
 	"fmt"
-	"bufio" //buffered IO package will have the scanner object
-	"log"
-	"os" //package to open outside files and release the file handler
+	//"bufio" //buffered IO package will have the scanner object
+	//"log"
+	//"os" //package to open outside files and release the file handler
 )
 
-var maze []string
-
-func loadMaze(file string) error {
-	//:= short-assignment operator that automatically infers the type of variable(s) based on the value(s) on the right hand side. commonly used in if, for, switch
-	f, err := os.Open(file) //Open function from os package which returns multiple values file and error (common Go pattern)
-	if err != nil { //nil means no value is assigned to a variable
-		return err
-	}
-	defer f.Close() //defer statement defers the execution of a function until the surrounding function returns. in this case we will close the file we opened. puts f.Close in the call stack
-	
-	scanner := bufio.NewScanner(f) //scanner object from bufio to read it to memory(global variable called maze). reads file line by line and appends it to maze slice (clean up purposes)
-	for scanner.Scan() { //scanner.Scan() will return true while there is something to be read from the file
-		line := scanner.Text() //returns the next line of input
-		maze = append(maze, line) //built-in function append adds a new element to the maze slice
-		//slices: lightweight ds, similar to array but more powerful. variable-length sequence which stores elements of similar type. Not a fixed size 
-		//a[low : high]
-	}
-	
-	return nil
-	//f.Close is called implicitly
-}
-
-func printScreen() {
+func printScreen(maze []string) { //array of whatever size
 	for _, line := range maze { //_ is a placeholder for where the compiler would expet a variable name. we are ignoring that value
 		//range - first return value is the index of the element starting from 0, second return value is the value itself
 		fmt.Println(line)
@@ -43,17 +21,19 @@ func printScreen() {
 func main() {
 	// initialize game
 
+	mazeloader := MazeLoader{};
 	// load resources
-	err := loadMaze("maze01.txt")
+	err := mazeloader.loadMaze("maze01.txt")
 	if err != nil {
-		log.Println("failed to load maze:", err)
+		fmt.Println("failed to load maze:", err)
 		return
 	}
 
 	// game loop never ending
 	for {
 		// update screen
-		printScreen()
+		maze := mazeloader.getmaze()
+		printScreen(maze)
 
 		// process input
 
