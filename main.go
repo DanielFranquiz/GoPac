773328e00d020bd11a1ce51e193755f737b78b85
Daniel Franquiz <dfranquiz561@gmail.com>
2020-03-10 20:07:16 -0400

    Added the ESC functionality

--- main.go
+++ main.go
@@ -4,12 +4,16 @@ package main
 //makes code in other packages accessible to this program
 import (
 	"fmt"
+	//"os/exec"
 	//"bufio" //buffered IO package will have the scanner object
-	//"log"
+	"log"
 	//"os" //package to open outside files and release the file handler
+	
+	"github.com/danicat/simpleansi"
 )
 
 func printScreen(maze []string) { //array of whatever size
+	simpleansi.ClearScreen()
 	for _, line := range maze { //_ is a placeholder for where the compiler would expet a variable name. we are ignoring that value
 		//range - first return value is the index of the element starting from 0, second return value is the value itself
 		fmt.Println(line)
@@ -20,6 +24,10 @@ func printScreen(maze []string) { //array of whatever size
 //main program takes no parameters and returns nothing
 func main() {
 	// initialize game
+	// initialise game
+    initialise()
+    defer cleanup()
+
 
 	mazeloader := MazeLoader{};
 	// load resources
@@ -36,17 +44,27 @@ func main() {
 		printScreen(maze)
 
 		// process input
-
+		// process input
+		input, err := readInput()
+		if err != nil {
+   			log.Print("error reading input:", err)
+			break
+		}
+		
 		// process movement
 
 		// process collisions
 
 		// check game over
-
+		if input == "ESC" {
+    		break
+		}
+		
+		
 		// Temp: break infinite loop
 		//fmt package formatted input/output
-		fmt.Println("Hello, Pac Go!")
-		break
+		//fmt.Println("Hello, Pac Go!")
+		//break
 
 		// repeat
 	}
