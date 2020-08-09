76e688a0452aa9a5b03ae7b4bdc0beff065eb256
LeoTheLegion <mmena2017@fau.edu>
2020-08-09 17:40:54 -0400

    We are able to check for Intersections. Added simple pathfindings.

--- Ghost.go
+++ Ghost.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"strconv"
+	"math"
 )
 
 var ghosts []*ghost
@@ -36,39 +37,124 @@ func captureGhostPosition(maze []string) {
 
 func moveGhosts(maze []string) {
     for _, g := range ghosts {
-		//check inter
-			//if inter
-				//set direction
+		
+		var x_diff,y_diff int = getVectorDifferenceFromPacManToGhost(g);
 
-		//g.dir = randomDirection()
-		var x, y int = GetDirectionToVector(g.dir)
+		/// B - A
+		// A == ghost
+		// B == pac
 
-		var wall = isWall(maze,g.position.row + y, g.position.col + x);
+		
+		xa := int(math.Abs(float64(x_diff)));
+		ya := int(math.Abs(float64(y_diff)));
 
-		debugLog(strconv.FormatBool(wall))
+		debugLog(strconv.Itoa(x_diff) + "," + strconv.Itoa(y_diff) +","+ g.dir);
 
-		if wall { // if we are at a wall
-			if g.dir == "UP" || g.dir == "DOWN"  { // if we are looking up or down
-				var xa, ya int = GetDirectionToVector("RIGHT")
-				if  !isWall(maze,g.position.row + ya, g.position.col + xa){ // decide left or right
-					g.dir = "RIGHT"
-				} else {
-					g.dir = "LEFT"
-				}
-			} else if g.dir == "RIGHT" || g.dir == "LEFT" { // if we are looking left or right
-				var xa, ya int = GetDirectionToVector("UP")
-				if  !isWall(maze,g.position.row + ya, g.position.col + xa){ // decide up or down
-					g.dir = "UP"
-				} else {
-					g.dir = "DOWN"
-				}
-			}
-		}
+		if(isAtIntersection(maze,g)){
 			
+
+			if(xa > ya){// x - axis
+				if(x_diff < 0){//we are left of him
+					g.dir = "LEFT";
+				}else{//we are right of him
+					g.dir = "RIGHT";
+				}
+				debugLog(strconv.Itoa(x_diff) + "," + strconv.Itoa(y_diff) +","+ g.dir + "," + "x");
+			}else{// y - axis
+				if(y_diff < 0){//below
+					g.dir = "UP";
+				}else{//above
+					g.dir = "DOWN";
+				}
+				debugLog(strconv.Itoa(x_diff) + "," + strconv.Itoa(y_diff) +","+ g.dir + "," + "y");
+			}
+			keepMovement(maze,g);// fixes problem a bit
+		}else{
+			keepMovement(maze,g);
+		}
+		
+		
 
 		g.position.row, g.position.col = makeMove(g.position.row, g.position.col, g.dir, maze)
 		//g.row, g.col = makeMove(g.row, g.col, dir, maze)
     }
+}
+
+func getVectorDifferenceFromPacManToGhost(g *ghost)(int,int){
+	var pacman sprite = player;
+
+	var x_diff,y_diff int = 0,0;
+
+	/// B - A
+	// A == ghost
+	// B == pac
+
+	x_diff = pacman.col - g.position.col;
+	y_diff = pacman.row - g.position.row;
+
+	return x_diff,y_diff;
+}
+
+func isAtIntersection(maze []string,g *ghost)(bool){
+
+		var count int = 0;
+
+		var x, y int = GetDirectionToVector("UP")
+		var wall = isWall(maze,g.position.row + y, g.position.col + x);
+
+		if(!wall){
+			count++;
+		}
+
+		x, y = GetDirectionToVector("DOWN")
+		wall = isWall(maze,g.position.row + y, g.position.col + x);
+
+		if(!wall){
+			count++;
+		}
+		x, y = GetDirectionToVector("RIGHT")
+		wall = isWall(maze,g.position.row + y, g.position.col + x);
+
+		if(!wall){
+			count++;
+		}
+
+		x, y = GetDirectionToVector("LEFT")
+		wall = isWall(maze,g.position.row + y, g.position.col + x);
+
+		if(!wall){
+			count++;
+		}
+
+		if(count > 2){
+			return true;
+		}
+
+		return false;
+}
+
+func keepMovement(maze []string,g *ghost){
+	var x, y int = GetDirectionToVector(g.dir)
+
+	var wall = isWall(maze,g.position.row + y, g.position.col + x);
+
+	if wall { // if we are at a wall
+		if g.dir == "UP" || g.dir == "DOWN"  { // if we are looking up or down
+			var xa, ya int = GetDirectionToVector("RIGHT")
+			if  !isWall(maze,g.position.row + ya, g.position.col + xa){ // decide left or right
+				g.dir = "RIGHT"
+			} else {
+				g.dir = "LEFT"
+			}
+		} else if g.dir == "RIGHT" || g.dir == "LEFT" { // if we are looking left or right
+			var xa, ya int = GetDirectionToVector("UP")
+			if  !isWall(maze,g.position.row + ya, g.position.col + xa){ // decide up or down
+				g.dir = "UP"
+			} else {
+				g.dir = "DOWN"
+			}
+		}
+	}
 }
 
 func ghostReturnHome(g *ghost){
